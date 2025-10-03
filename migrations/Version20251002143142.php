431f89a73ef76f450a5bd67783de95a4eaaea209
BallesteroDavid <david.ballestero@outlook.fr>
2025-10-03 10:56:20 +0200

    user_owner_validation_done

--- migrations/Version20251002143142.php
+++ migrations/Version20251002143142.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20251002124815 extends AbstractMigration
+final class Version20251002143142 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,6 +20,7 @@ final class Version20251002124815 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD name VARCHAR(180) NOT NULL');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D6495E237E06 ON user (name)');
     }
 
@@ -27,5 +28,6 @@ final class Version20251002124815 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP INDEX UNIQ_8D93D6495E237E06 ON user');
+        $this->addSql('ALTER TABLE user DROP name');
     }
 }
